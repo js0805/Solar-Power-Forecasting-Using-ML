5ee3df4bf254dcfb0161aa14e8d40ef9a8984d76
Jatin Sharma <52196885+js0805@users.noreply.github.com>
2020-05-19 00:23:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-"""
-author : @Js0805
-"""
 import warnings
 import itertools
 import numpy as np
@@ -28,57 +25,3 @@ regressor.fit(X_train,y_train)
 y_pred = regressor.predict(X_test)
 
 
-dataset_1= pd.read_excel('dataset.xlsx',sheet_name='Sheet1')
-cols =['AT','WS','RH','BP','SR']
-dataset_1.drop(cols, axis=1, inplace=True)
-
-dataset_1 = dataset_1.sort_values('Date')
-
-dataset_1.isnull().sum()
-
-dataset_1 = dataset_1.groupby('Date')['Energy (Meters)'].sum().reset_index()
-
-dataset_1 = dataset_1.set_index('Date')
-dataset_1.index
-
-
-a = dataset_1['Energy (Meters)'].resample('MS').mean()
-a.plot(figsize=(15, 6))
-plt.show()
-
-p = d = q = range(0, 2)
-pdq = list(itertools.product(p, d, q))
-seasonal_pdq = [(x[0], x[1], x[2], 12) for x in list(itertools.product(p, d, q))]
-
-print('Examples of parameter combinations for Seasonal ARIMA...')
-print('SARIMAX: {} x {}'.format(pdq[1], seasonal_pdq[1]))
-print('SARIMAX: {} x {}'.format(pdq[1], seasonal_pdq[2]))
-print('SARIMAX: {} x {}'.format(pdq[2], seasonal_pdq[3]))
-print('SARIMAX: {} x {}'.format(pdq[2], seasonal_pdq[4]))
-
-import warnings
-warnings.filterwarnings("ignore")
-for param in pdq:
-    for param_seasonal in seasonal_pdq:
-        try:
-            mod = sm.tsa.statespace.SARIMAX(a,
-                                            order=param,
-                                            seasonal_order=param_seasonal,
-                                            enforce_stationarity=False,
-                                            enforce_invertibility=False)
-
-            results = mod.fit()
-
-            print('ARIMA{}x{}12 - AIC:{}'.format(param, param_seasonal, results.aic))
-        except:
-            continue
-        
-mod = sm.tsa.statespace.SARIMAX(y,
-                                order=(1, 0, 0),
-                                seasonal_order=(0, 0, 0, 12),
-                                enforce_stationarity=False,
-                                enforce_invertibility=False)
-
-results = mod.fit()
-
-print(results.summary().tables[1])
